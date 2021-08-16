17e34851a92081947f140d25cd1bfc1cd5758b6b
Aman Chaudhary <achoudhary@deqode.com>
2021-08-16 16:06:17 +0530

    node_build in modules

---
+++ module1/os.js
@@ -0,0 +1,16 @@
+const os = require('os');
+
+console.log(os.arch())
+
+const freeMemo = os.freemem();
+
+console.log(` free memory => ${freeMemo} bytes`);
+
+const totalMemo = os.totalmem();
+
+console.log(`total memory ==> ${totalMemo} bytes`);
+
+
+console.log(`Host name => ${os.hostname()}`)
+
+console.log(`platform ==> ${os.platform()}`)
