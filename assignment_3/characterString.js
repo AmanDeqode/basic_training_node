6653d918964575eddfe0e48ce5b15d03a2980b24
Aman Chaudhary <achoudhary@deqode.com>
2021-08-26 11:19:27 +0530

    clean the code of characterString.js

--- assignment_3/characterString.js
+++ assignment_3/characterString.js
@@ -1,62 +1,18 @@
-// let str = 'deqode industry'
+let str = 'deqode industry'
 
-// str = str.split(' ');
+str = str.split(' ');
 
-// let arr = [];
+let arr = [];
 
-// str.forEach((s)=> {
-//     const firstChar = s.charAt(0);
+str.forEach((s)=> {
+    const firstChar = s.charAt(0);
 
-//     const newStr = s.replace(firstChar,firstChar.toUpperCase());
+    const newStr = s.replace(firstChar,firstChar.toUpperCase());
 
-//     arr.push(newStr);
-// });
+    arr.push(newStr);
+});
 
-// console.log(arr.join(' '));
+console.log(arr.join(' '));
 
-const arr = [
-    {
-      name: 'D',
-      technologies: [{
-        name: 'JAVA',
-        experience: '2 years'
-      },
-      {
-        name: 'Javascript',
-        experience: '5 years',
-      }]
-    },
-    {
-      name: 'C',
-      technologies: [{
-        name: 'NodeJs',
-        experience: '5 years'
-      },
-      {
-        name: 'Typescript',
-        experience: '10 years',
-      }]
-    },
-    {
-      name: 'K',
-      technologies: [{
-        name: 'CPP',
-        experience: '10 years'
-      },
-      {
-        name: 'Python',
-        experience: '10 years',
-      }]
-    }
-  ]
-
-
- const technologies = arr.flatMap((item)=> item.technologies)
-
- console.log(technologies);
-
-
- 
- //console.log(arr.flat());
 
 
