9e00d6a71ce29a923bcf12695d666c9abc5998f6
Aman Chaudhary <achoudhary@deqode.com>
2021-09-01 10:49:47 +0530

    made relevant changes

--- assignment_4/credit-card.js
+++ assignment_4/credit-card.js
@@ -11,10 +11,27 @@ const askNumber = (enterNumber) =>{
     });
 }
 
+const regExpVisa = /^4[0-9]{12}(?:[0-9]{3})/g;
+
+const regExpRupay = /^6[0-9]{12}(?:[0-9]{3})/g;
+
 const creditCardPattern = async() =>{
+
     const number = await askNumber('Have a trust on me and provide credit card number:  ')
 
-    console.log(number);
+    if(number===regExpVisa)
+    {
+        console.log('You have a visa card');
+        rl.close();
+    }
+    else if(number===regExpRupay)
+    {
+        console.log('You have a rupay card')
+    }
+    else
+    {
+        console.log('Sorry your card functionality didn\'t persent in our database' );
+    }
 } 
 
 
