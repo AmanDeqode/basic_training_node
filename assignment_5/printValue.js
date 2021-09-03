745d210c05822bccf4bea574a860bec14ac8db6b
Aman Chaudhary <achoudhary@deqode.com>
2021-09-03 14:15:05 +0530

    printValue.js

--- assignment_5/printValue.js
+++ assignment_5/printValue.js
@@ -1,15 +1,19 @@
 const numbers = [1,2,3,4,5,6,7,8,9,10];
 
-numbers.forEach((number)=>{
-    randomNumber(number)
-});
+let num = 0;
+let delay = 1000;
 
-function randomNumber(number){
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve(number)
-        },Math.random()*6000)
-    }).then((digit)=>{
-        console.log(digit)
-    });
+function randomInterval(num){
+    if(num=== numbers.length)
+    {
+        return;
+    }
+    setTimeout(()=>{
+        console.log(numbers[num]);
+        num++;
+        randomInterval(num);
+    },delay)
+    delay = Math.floor(Math.random()*(6-0+1))*1000
 }
+
+randomInterval(num);
